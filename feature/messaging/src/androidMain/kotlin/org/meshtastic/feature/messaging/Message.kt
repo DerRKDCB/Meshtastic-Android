d1265d7c0fe35c5d68b3d97f38b0feae0da523d9
owner <owner@mail.com>
2026-03-20 18:04:33 +0100

    Disable attachment menu if node not connected

--- feature/messaging/src/androidMain/kotlin/org/meshtastic/feature/messaging/Message.kt
+++ feature/messaging/src/androidMain/kotlin/org/meshtastic/feature/messaging/Message.kt
@@ -1011,6 +1011,12 @@ private fun MessageInput(
 
     var showAttachmentMenu by remember { mutableStateOf(false) }
 
+    LaunchedEffect(isEnabled) {
+        if (!isEnabled) {
+            showAttachmentMenu = false
+        }
+    }
+
     var selectedImageUri by remember { mutableStateOf<Uri?>(null) }
 
     var selectedSize by remember { mutableStateOf(128) }
@@ -1030,7 +1036,14 @@ private fun MessageInput(
         isError = isOverLimit,
         placeholder = { Text(stringResource(Res.string.type_a_message)) },
         leadingIcon = {
-            IconButton(onClick = { showAttachmentMenu = true }) {
+            IconButton(
+                onClick = {
+                    if (isEnabled) {
+                        showAttachmentMenu = true
+                    }
+                },
+                enabled = isEnabled,
+            ) {
                 Icon(
                     imageVector = Icons.Filled.AttachFile,
                     contentDescription = stringResource(Res.string.attachment),
@@ -1069,7 +1082,7 @@ private fun MessageInput(
     )
 
     DropdownMenu(
-        expanded = showAttachmentMenu,
+        expanded = showAttachmentMenu && isEnabled,
         onDismissRequest = { showAttachmentMenu = false }
     ) {
         DropdownMenuItem(
